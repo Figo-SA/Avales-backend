6211c732412d4cc4af092871c20a4d3fa9b9317e
Cristhianjvg <cristhianvillamarin2002@gmail.com>
2025-10-02 18:51:15 -0500

    features: eliminar todos los modulos sin usar

--- src/modules/certificado-financiero/dto/update-certificado-financiero.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateCertificadoFinancieroDto } from './create-certificado-financiero.dto';
-
-export class UpdateCertificadoFinancieroDto extends PartialType(CreateCertificadoFinancieroDto) {}
--- src/modules/revision-dtm/revision-dtm.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RevisionDtmService } from './revision-dtm.service';
-import { RevisionDtmController } from './revision-dtm.controller';
-
-@Module({
-  controllers: [RevisionDtmController],
-  providers: [RevisionDtmService],
-})
-export class RevisionDtmModule {}
