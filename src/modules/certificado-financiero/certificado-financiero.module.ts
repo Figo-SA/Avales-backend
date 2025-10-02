f49f7a43e87a12f2f6ed61cafdf7defc7073591d
Cristhianjvg <cristhianvillamarin2002@gmail.com>
2025-10-02 18:51:15 -0500

    features: eliminar todos los modulos sin usar

--- src/modules/certificado-financiero/certificado-financiero.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CertificadoFinancieroService } from './certificado-financiero.service';
-import { CertificadoFinancieroController } from './certificado-financiero.controller';
-
-@Module({
-  controllers: [CertificadoFinancieroController],
-  providers: [CertificadoFinancieroService],
-})
-export class CertificadoFinancieroModule {}
--- src/modules/certificado-pda/dto/update-certificado-pda.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateCertificadoPdaDto } from './create-certificado-pda.dto';
-
-export class UpdateCertificadoPdaDto extends PartialType(CreateCertificadoPdaDto) {}
