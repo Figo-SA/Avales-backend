5d761333e9b723d5360ed5a1344b0a3e76464a03
Cristhianjvg <cristhianvillamarin2002@gmail.com>
2025-10-03 10:05:15 -0500

    refactor: Eliminar entity de usuarios y separar response de base user

--- src/modules/users/dto/update-user.dto.ts
+++ src/modules/users/dto/update-user.dto.ts
@@ -1,5 +1,29 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateUserDto } from './create-user.dto';
-import { BaseUserDto } from './base-user.dto';
+// dto/update-user.dto.ts
+import { PartialType, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsOptional,
+  IsString,
+  Length,
+  IsArray,
+  IsInt,
+  Min,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+import { UserEditableDto } from './user-editable.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(UserEditableDto) {
+  @ApiPropertyOptional({ example: 'newpass123', minLength: 6, maxLength: 32 })
+  @IsOptional()
+  @IsString()
+  @Length(6, 32)
+  password?: string;
+
+  // Si permites cambiar roles en update:
+  @ApiPropertyOptional({ example: [3, 7] })
+  @IsOptional()
+  @Type(() => Number)
+  @IsArray()
+  @IsInt({ each: true })
+  @Min(1, { each: true })
+  rolIds?: number[];
+}
