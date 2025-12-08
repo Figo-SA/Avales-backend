96b9a454bf40346fa46279f976ec8d1c6aacb8b1
Cristhianjvg <cristhianvillamarin2002@gmail.com>
2025-12-08 16:31:38 -0500

    fix: tomar el token no del header sino directamente del cookie

--- src/modules/auth/guards/jwt-auth.guard.ts
+++ src/modules/auth/guards/jwt-auth.guard.ts
@@ -1,6 +1,21 @@
 // src/modules/auth/guards/jwt-auth.guard.ts
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  getRequest(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+
+    // Si el token NO está en Authorization, lo buscamos en la cookie:
+    if (
+      !request.headers['authorization'] &&
+      request.cookies &&
+      request.cookies.auth_token
+    ) {
+      request.headers['authorization'] = `Bearer ${request.cookies.auth_token}`;
+    }
+
+    return request;
+  }
+}
